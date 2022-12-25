87f237c66d3eee767b99cae12a5d7b08b63e0d3d
Rajkumar_R <61075470+Rajkumar-21@users.noreply.github.com>
2022-12-25 18:18:50 +0530

    Add files via upload

---
+++ Get-UserGroup.ps1
@@ -0,0 +1,56 @@
+﻿# Import the ActiveDirectory module
+Import-Module ActiveDirectory
+
+# Define the path to the CSV file
+$users = 'C:\Users\rajkumar\Documents\Owners.csv'
+
+$results = Import-Csv $users | Foreach-Object {
+    # Check if the Username column is defined or empty
+    if (!$_.Username -or $_.Username -eq '') {
+        # Output a message if the username is not defined in the CSV file
+        [PSCustomObject]@{
+            'Username' = 'not defined'
+            'Email' = 'unknown'
+            'Group' = 'unknown'
+            'Status' = 'not defined in CSV file'
+            'Action' = ''
+        }
+    } else {
+        # Get the username and group name from the current row
+        $username = $_.Username
+        $groupName = $_.Group
+        # Get the user object for the current username
+        $user = Get-ADUser -Filter {SamAccountName -eq $username}
+        if ($user) {
+            # Get the email address of the user
+            $email = $user.EmailAddress
+            $isMember = [bool](Get-ADGroupMember -Identity $groupName -Recursive | Where-Object { $_.SamAccountName -eq $user.SamAccountName })
+
+            # Output the results
+            if ($isMember) {
+                [PSCustomObject]@{
+                    'Username' = $username
+                    'Email' = $email
+                    'Group' = $groupName
+                    'Status' = 'Present'
+                }
+            } else {
+                [PSCustomObject]@{
+                    'Username' = $username
+                    'Email' = $email
+                    'Group' = $groupName
+                    'Status' = 'Not Present'
+                }
+            }
+        } else {
+            [PSCustomObject]@{
+                'Username' = $username
+                'Email' = 'unknown'
+                'Group' = $groupName
+                'Status' = 'unknown'
+            }
+        }
+    }
+}
+
+$results | Export-Csv -Path 'C:\Users\rajkumar\Documents\access\OwnersReport.csv' -NoTypeInformation
---
+++ Get-VMDetails.ps1
@@ -0,0 +1,41 @@
+﻿# Import the AzureRM module
+#Import-Module AzureRM
+
+# Connect to Azure
+Connect-AzureRmAccount -Credential $Credential
+
+# Set the resource group name
+$resourceGroup = "RG"
+
+# Get all the VMs in the resource group
+$vms = Get-AzureRMVM -ResourceGroupName $resourceGroup
+
+# Create an empty array to store the VM info
+$vmInfo = @()
+
+# Loop through each VM
+foreach($vm in $vms)
+{
+    # Get the VM's spec and tags
+    $vmSpec = $vm.HardwareProfile
+    $vmTags = $vm.Tags
+
+    # Check if the VM is connected to Azure Monitor and Log Analytics
+    $isConnectedToMonitor = $vm.DiagnosticsProfile.BootDiagnostics.Enabled
+    $isConnectedToLogAnalytics = $vm.DiagnosticsProfile.LogAnalytics.WorkspaceId -ne $null
+
+    # Create an object to store the VM info
+    $vmData = [pscustomobject]@{
+        "VM Name" = $vm.Name
+        "VM Spec" = $vmSpec
+        "VM Tags" = $vmTags
+        "Connected to Azure Monitor" = $isConnectedToMonitor
+        "Connected to Log Analytics" = $isConnectedToLogAnalytics
+    }
+
+    # Add the object to the array
+    $vmInfo += $vmData
+}
+
+# Export the array to an Excel file
+$vmInfo | Export-Excel -Path "C:\Users\rajkumar\Documents\Reports\AzureVM.csv"
