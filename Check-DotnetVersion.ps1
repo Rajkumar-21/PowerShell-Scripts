3a4827ab56e4c3377107f380cc01c3622245cc16
Rajkumar_R <61075470+Rajkumar-21@users.noreply.github.com>
2023-01-05 17:11:10 +0530

    Add files via upload

---
+++ Check-DotnetVersion.ps1
@@ -0,0 +1,34 @@
+﻿# Get a list of all the installed versions of the .NET Framework on the system
+$dotNetVersions = Get-ChildItem 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP' -Recurse |
+    Get-ItemProperty -Name Version, Release -ErrorAction SilentlyContinue |
+    Where-Object { $_.PSChildName -Match '^(?!S)\p{L}' } |
+    Select-Object Version, Release
+
+# Check if any of the installed .NET Framework versions have a version number equal to or higher than 4.8
+$dotNet48OrHigherInstalled = $false
+$dotNetLatestVersion = [System.Version]::Parse('4.8')
+foreach ($dotNetVersion in $dotNetVersions) {
+if ([System.Version]::Parse($dotNetVersion.Version) -ge [System.Version]::Parse('4.8')) {
+    $dotNet48OrHigherInstalled = $true
+    # Check if the current version is higher than the previously stored version
+    $VMdotNetVersion = $dotNetVersion.Version
+}
+}
+  
+# If a version of .NET Framework 4.8 or higher is not installed, proceed with the installation
+if (!$dotNet48OrHigherInstalled)
+{
+    # Perform action for "dotnet"
+    if(Test-Path -Path "C:\temp\Softwares\ndp48-web.exe"){
+    #Start-Process "C:\temp\Softwares\ndp48-web.exe" -ArgumentList "/q /norestart" -wait -PassThru -Verbose
+    Write-host "Installation of dotnet completed" -foregroundColor Green
+    }
+    else{
+    Write-Host "Unable to find: C:\temp\Softwares\ndp48-web.exe" -ForegroundColor Red
+    }
+}
+else{
+Write-Host "Dotnet already installed on this VM and version: $VMdotNetVersion"
+    
+}
+
---
+++ Disk-Details.ps1
@@ -0,0 +1,6 @@
+﻿$drive = Get-WmiObject Win32_LogicalDisk -Filter "DeviceID='E:'"
+$capacity = "{0:N2}" -f ($drive.Size / 1GB)
+$used = "{0:N2}" -f (($drive.Size - $drive.FreeSpace) / 1GB)
+$free = "{0:N2}" -f ($drive.FreeSpace / 1GB)
+
+Write-Host "Total capacity: $capacity GB`nUsed capacity: $used GB`nFree space: $free GB"
